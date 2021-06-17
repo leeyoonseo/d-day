498d9ddd57264db21b20e88edcf79ef17aee3199
leeyoonseo <okayoon.lee@gmail.com>
2021-06-17 23:29:20 +0900

    프로젝트 구조 구상 중ㅎㅎ!! scss 설정 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/Header';
 import Container from './components/Container';
 import Footer from './components/Footer';
 
